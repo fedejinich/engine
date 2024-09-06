ff0e1c46d81f772c4acb17bf60beab825f244dc4
Kirk Scheibelhut <kjs@scheibo.com>
2024-09-05 22:45:03 -0700

    prune fields taking into consideration formechanges and metronome

--- src/tools/display/index.ts
+++ src/tools/display/index.ts
@@ -7,7 +7,9 @@ import * as pkmn from '@pkmn/data';
 import * as esbuild from 'esbuild';
 import {minify} from 'html-minifier';
 
+import * as addon from '../../pkg/addon';
 import * as data from '../../pkg/data';
+import * as gen1 from '../../pkg/gen1';
 
 export * from './util';
 
@@ -21,17 +23,35 @@ export function error(err: string) {
     .split('\n').slice(0, -3).join('\n'));
 }
 
-export function render(gens: pkmn.Generations, buf: Buffer, err?: string, seed?: bigint) {
-  if (!buf.length) throw new Error('Invalid input');
+export function render(gens: pkmn.Generations, buffer: Buffer, err?: string, seed?: bigint) {
+  if (!buffer.length) throw new Error('Invalid input');
 
-  const view = data.Data.view(buf);
-  const gen = gens.get(view.getUint8(1));
+  const view = data.Data.view(buffer);
+
+  let offset = 0;
+  const showdown = !!view.getUint8(offset++);
+  const gen = gens.get(view.getUint8(offset++));
+  offset += 2;
+
+  const lookup = data.Lookup.get(gen);
+  const size = data.LAYOUT[gen.num - 1].sizes.Battle;
+  const deserialize = (buf: Buffer) => {
+    // We don't care about the native addon, we just need to load it so other checks don't fail
+    void addon.supports(true);
+    switch (gen.num) {
+      case 1: return new gen1.Battle(lookup, data.Data.view(buf), {showdown});
+      default: throw new Error(`Unsupported gen: ${gen.num}`);
+    }
+  };
+
+  const battle = deserialize(buffer.subarray(offset, offset += size));
 
   const json: {
     num: pkmn.GenerationNum;
     species: {[id: string]: {
       name: pkmn.SpeciesName;
       num: number;
+      // TODO: technically these gender fields are useless in gen 1
       genderRatio: {M: number; F: number};
       gender?: pkmn.GenderName;
     };};
@@ -44,24 +64,47 @@ export function render(gens: pkmn.Generations, buf: Buffer, err?: string, seed?:
     };};
   } = {num: gen.num, species: {}, moves: {}};
 
-  // metronome and forme changes fuck us
-
-  for (const species of gen.species) {
-    json.species[species.id] = {
-      name: species.name,
-      num: species.num,
-      genderRatio: species.genderRatio,
-      gender: species.gender,
-    };
+  let metronome = false;
+  for (const side of battle.sides) {
+    for (const pokemon of side.pokemon) {
+      const s = gen.species.get(pokemon.species)!;
+      for (const forme of [s.id, ...(s.formes ?? [])]) {
+        const species = gen.species.get(forme)!;
+        json.species[species.id] = {
+          name: species.name,
+          num: species.num,
+          genderRatio: species.genderRatio,
+          gender: species.gender,
+        };
+      }
+      if (!metronome) {
+        for (const ms of pokemon.moves) {
+          if (ms.id === 'metronome') {
+            metronome = true;
+            break;
+          }
+          const move = gen.moves.get(ms.id)!;
+          json.moves[move.id] = {
+            name: move.name,
+            num: move.num,
+            maxpp: Math.min(move.pp / 5 * 8, gen.num === 1 ? 61 : 64),
+            basePower: move.basePower,
+            type: move.type,
+          };
+        }
+      }
+    }
   }
-  for (const move of gen.moves) {
-    json.moves[move.id] = {
-      name: move.name,
-      num: move.num,
-      maxpp: Math.min(move.pp / 5 * 8, gen.num === 1 ? 61 : 64),
-      basePower: move.basePower,
-      type: move.type,
-    };
+  if (metronome) {
+    for (const move of gen.moves) {
+      json.moves[move.id] = {
+        name: move.name,
+        num: move.num,
+        maxpp: Math.min(move.pp / 5 * 8, gen.num === 1 ? 61 : 64),
+        basePower: move.basePower,
+        type: move.type,
+      };
+    }
   }
 
   // we want to use the transform API but also want to bundle
@@ -89,7 +132,7 @@ export function render(gens: pkmn.Generations, buf: Buffer, err?: string, seed?:
     <script>
     window.DATA = ${JSON.stringify({
     gen: json,
-    buf: buf.toString('base64'),
+    buf: buffer.toString('base64'),
     error: err && error(err),
     seed: seed && `${seed}n`,
   })};
