b8983c000793ed18c3de0e4a5cc7c27458da6fb8
Kirk Scheibelhut <kjs@scheibo.com>
2024-09-10 16:02:55 -0700

    improve sort orders

--- src/tools/demo.ts
+++ src/tools/demo.ts
@@ -36,37 +36,95 @@ const options = {
   log: false,
 };
 
-// const SKIP = ['gen1lc'] as ID[];
+const SKIP = ['gen1lc'] as ID[];
 
-(() => {
-  const order: number[] = [];
-  const species: {[id: string]: Species} = {};
-  for (const s of gen.species) {
-    species[s.id] = pruneSpecies(gen, s);
-    // let moves: string[];
-    // try {
-    //   if (SKIP.includes(Smogon.format(gen, s)!)) throw new Error();
-    //   const stats = await smogon.stats(gen, s);
-    //   moves = Object.keys(stats!.moves);
-    // } catch {
-    //   moves = (await smogon.sets(gen, s))[0]?.moves ??
-    //   Object.keys((await gen.learnsets.learnable(s.name))!);
-    // }
-    // order.push(...moves
-    //   .filter(m => m !== 'Nothing')
-    //   .slice(0, 20)
-    //   .map(m => lookup.moveByID(toID(m))),
-    // 0);
+(async () => {
+  const order: {
+    global: {species: {[id: string]: number}; moves: {[id: string]: number}};
+    local: number[];
+  } = {global: await overall(), local: []};
+
+  const data: {
+    num: number;
+    species: {[id: string]: Species};
+    moves: {[id: string]: Move};
+  } = {num: gen.num, species: {}, moves: {}};
+
+  for (const id in order.global.species) {
+    const s = gen.species.get(id)!;
+    data.species[s.id] = pruneSpecies(gen, s);
+    let usage: string[];
+    try {
+      if (SKIP.includes(Smogon.format(gen, s)!)) throw new Error();
+      const stats = await smogon.stats(gen, s);
+      usage = Object.keys(stats!.moves).filter(m => m !== 'Nothing');
+    } catch {
+      usage = (await smogon.sets(gen, s))[0]?.moves ?? [];
+    }
+
+    const learnset = [];
+    for (const move in (await gen.learnsets.learnable(s.name))!) {
+      if (!usage.includes(move)) learnset.push(move);
+    }
+    learnset.sort((a, b) => order.global.moves[b] - order.global.moves[a]);
+
+    order.local.push(...([...usage, ...learnset]).map(m => lookup.moveByID(toID(m))), 0);
   }
-  const moves: {[id: string]: Move} = {};
-  for (const m of gen.moves) moves[m.id] = pruneMove(gen, m);
+  for (const m of gen.moves) data.moves[m.id] = pruneMove(gen, m);
 
   const battle = Battle.create(gen, options);
   battle.update(Choice.pass, Choice.pass);
   process.stdout.write(render(path.join(ROOT, 'build', 'tools', 'display', 'demo.jsx'), {
-    order: Buffer.from(order).toString('base64'),
-    gen: {num: gen.num, species, moves},
+    order: {
+      species: Buffer.from(Object.keys(order.global.species)
+        .map(s => lookup.speciesByID(s as ID))).toString('base64'),
+      moves: Buffer.from(order.local).toString('base64'),
+    },
+    gen: data,
     buf: Buffer.from((battle as any).data.buffer).toString('base64'),
     showdown,
   }, {styles: [path.join(ROOT, 'src', 'tools', 'display', 'autocomplete.css')]}));
 })();
+
+const TIERS = Object.fromEntries([
+  'AG', 'Uber', '(Uber)',
+  'OU', '(OU)', 'UUBL',
+  'UU', 'RUBL', 'RU',
+  'NUBL', 'NU', '(NU)',
+  'PUBL', 'PU', '(PU)',
+  'ZUBL', 'ZU', 'NFE', 'LC',
+  'Unreleased', 'Illegal', 'CAP',
+  'CAP NFE', 'CAP LC',
+].map((t, i) => [t, i]));
+
+function sort(
+  weights: {[id: string]: number},
+  cmp = (a: [string, number], b: [string, number]) => b[1] - a[1],
+) {
+  return Object.fromEntries(Object.entries(weights).sort(cmp).map((a, i) => [a[0], i]));
+}
+
+async function overall() {
+  const species: {[id: string]: number} = {};
+  const moves: {[id: string]: number} = {};
+
+  for (const s of gen.species) {
+    try {
+      const stats = (await smogon.stats(gen, s, `gen${gen.num}ou` as ID))!;
+      species[s.id] = stats.usage.weighted!;
+      for (const m in stats.moves) {
+        if (m === 'Nothing') continue;
+        const id = toID(m);
+        moves[id] = (moves[id] || 0) + (stats.moves[m] * stats.usage.weighted);
+      }
+    } catch {
+      species[s.id] = 0;
+    }
+  }
+
+  // Sort first by usage followed by tier
+  const sorted = sort(species, (a, b) => b[1] - a[1] ||
+    ((TIERS[gen.species.get(a[0])!.tier] ?? Infinity) -
+      (TIERS[gen.species.get(b[0])!.tier] ?? Infinity)));
+  return {species: sorted, moves: sort(moves)};
+}
--- src/tools/display/demo.tsx
+++ src/tools/display/demo.tsx
@@ -25,17 +25,24 @@ const json = (window as any).DATA;
 const GEN = adapt(new Gen(json.gen));
 
 const lookup = engine.Lookup.get(GEN);
-const raw = atob(json.order);
-const order: {[id: string]: string[]} = {};
-let i = 0;
-for (const s of GEN.species) {
-  const moves = [];
-  for (; i < raw.length && raw.charCodeAt(i) !== 0; i++) {
-    moves.push(lookup.moveByNum(raw.charCodeAt(i)));
+const order: {species: string[]; moves: {[id: string]: string[]}} = {species: [], moves: {}};
+
+let offset = 0;
+const species = atob(json.order.species);
+const moves = atob(json.order.moves);
+for (let i = 0; i < species.length; i++) {
+  const id = lookup.speciesByNum(species.charCodeAt(i));
+  const specie = GEN.species.get(id)!;
+  order.species.push(specie.name);
+
+  const ids = [];
+  for (; offset < moves.length && moves.charCodeAt(offset) !== 0; offset++) {
+    ids.push(lookup.moveByNum(moves.charCodeAt(offset)));
   }
-  order[s.id] = moves;
-  i++;
+  offset++;
+  order.moves[specie.id] = ids;
 }
+
 console.debug(order);
 
 // const buf = Uint8Array.from(atob(json.buf), c => c.charCodeAt(0));
@@ -45,20 +52,23 @@ console.debug(order);
 //   showdown={json.showdown}
 // />);
 
-const SpeciesSelect = ({gen}: {gen: Generation}) => {
+const SpeciesSelect = () => {
   // const options = Array.from(gen.species).map(s => <option value={s.id}>{s.name}</option>);
   // return <select name="species" id="species">{options}</select>;
 
-  const choices = Array.from(gen.species).map(s => s.name);
-  const searcher = new FuzzySearch({source: choices, token_field_min_length: 1});
+  const searcher = new FuzzySearch({
+    source: order.species,
+    token_field_min_length: 1,
+    sorter: (a: any, b: any) => b.score - a.score,
+  });
 
   const input = <input type="text" name="q" placeholder="Tauros" ></input>;
   const _ = new AutoComplete(input,
     function (term, suggest) {
       suggest(searcher.search(term));
-    }, {minChars: 1,
-
-      renderItem(item: string, search: string) {
+    }, {
+      minChars: 1,
+      renderItem(item: string) {
         return '<div class="autocomplete-suggestion" data-val="' + item + '">' +
         searcher.highlight(item) + '</div>';
       },
@@ -66,4 +76,4 @@ const SpeciesSelect = ({gen}: {gen: Generation}) => {
   return input;
 };
 
-document.getElementById('content')!.appendChild(<SpeciesSelect gen={GEN} />);
+document.getElementById('content')!.appendChild(<SpeciesSelect />);
