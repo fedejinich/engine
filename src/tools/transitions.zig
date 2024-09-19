d6daf1e621059310c09bebb60b246c767d9282fd
Kirk Scheibelhut <kjs@scheibo.com>
2024-09-18 20:09:14 -0700

    make transitions understand full format

--- src/tools/transitions.zig
+++ src/tools/transitions.zig
@@ -3,14 +3,14 @@ const std = @import("std");
 
 const pkmn = @import("pkmn");
 
+const showdown = pkmn.options.showdown;
+
 const move = pkmn.gen1.helpers.move;
 const swtch = pkmn.gen1.helpers.swtch;
 
 pub const pkmn_options = pkmn.Options{ .internal = true };
 
 const debug = false; // DEBUG
-
-const SIZES = [_]usize{@sizeOf(pkmn.gen1.Battle(pkmn.gen1.PRNG))};
 
 pub fn main() !void {
     std.debug.assert(pkmn.options.calc and pkmn.options.chance);
@@ -21,7 +21,7 @@ pub fn main() !void {
 
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
-    if (args.len < 2 or args.len > 3) usageAndExit(args[0]);
+    if (args.len != 1 and (args.len < 2 or args.len > 3)) usageAndExit(args[0]);
 
     var buf: [pkmn.LOGS_SIZE]u8 = undefined;
     var stream = pkmn.protocol.ByteStream{ .buffer = &buf };
@@ -29,11 +29,11 @@ pub fn main() !void {
     const out = std.io.getStdOut().writer();
     // const out = std.io.null_writer;
 
-    const gen = std.fmt.parseUnsigned(u8, args[1], 10) catch
-        errorAndExit("gen", args[1], args[0]);
-    if (gen < 1 or gen > 9) errorAndExit("gen", args[1], args[0]);
-
     if (args.len > 2) {
+        const gen = std.fmt.parseUnsigned(u8, args[1], 10) catch
+            errorAndExit("gen", args[1], args[0]);
+        if (gen < 1 or gen > 9) errorAndExit("gen", args[1], args[0]);
+
         const seed = if (args.len > 2) try std.fmt.parseUnsigned(u64, args[2], 0) else 0x1234568;
 
         // const PAR = pkmn.gen1.Status.init(.PAR);
@@ -95,6 +95,24 @@ pub fn main() !void {
         var reader = std.io.bufferedReader(stdin.reader());
         var r = reader.reader();
 
+        if (try r.readByte() != @intFromBool(showdown)) {
+            const err = std.io.getStdErr().writer();
+            err.print("Cannot process frame from -Dshowdown={}\n", .{!showdown}) catch {};
+            usageAndExit(args[0]);
+        }
+
+        const gen = try r.readByte();
+        if (gen < 1 or gen > 9) errorAndExit("gen", gen, args[0]);
+
+        var zero = try r.readInt(u16, builtin.cpu.arch.endian());
+        if (zero != 0) errorAndExit("log size", zero, args[0]);
+
+        _ = try switch (gen) {
+            1 => r.readStruct(pkmn.gen1.Battle(pkmn.gen1.PRNG)),
+            else => unreachable,
+        };
+        zero = try r.readByte();
+        if (zero != 0) errorAndExit("log data", zero, args[0]);
         const battle = try switch (gen) {
             1 => r.readStruct(pkmn.gen1.Battle(pkmn.gen1.PRNG)),
             else => unreachable,
@@ -132,9 +150,9 @@ fn format(gen: u8, stream: *pkmn.protocol.ByteStream) void {
     stream.reset();
 }
 
-fn errorAndExit(msg: []const u8, arg: []const u8, cmd: []const u8) noreturn {
+fn errorAndExit(msg: []const u8, arg: anytype, cmd: []const u8) noreturn {
     const err = std.io.getStdErr().writer();
-    err.print("Invalid {s}: {s}\n", .{ msg, arg }) catch {};
+    err.print("Invalid {s}: {any}\n", .{ msg, arg }) catch {};
     usageAndExit(cmd);
 }
 
