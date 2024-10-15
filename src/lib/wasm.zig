a8e62faa80c80d7013c224d772ea087c28b4723c
Kirk Scheibelhut <kjs@scheibo.com>
2024-10-15 14:51:23 -0700

    prelim attempts to export wasm fns...

--- src/lib/wasm.zig
+++ src/lib/wasm.zig
@@ -1,6 +1,10 @@
 const pkmn = @import("pkmn.zig");
 const std = @import("std");
 
+const assert = std.debug.assert;
+
+const Enum = if (@hasField(std.builtin.Type, "enum")) .@"enum" else .Enum;
+
 export const SHOWDOWN = pkmn.options.showdown;
 export const LOG = pkmn.options.log;
 export const CHANCE = pkmn.options.chance;
@@ -10,3 +14,35 @@ export const GEN1_CHOICES_SIZE =
     std.math.ceilPowerOfTwo(u32, @as(u32, @intCast(pkmn.gen1.CHOICES_SIZE))) catch unreachable;
 export const GEN1_LOGS_SIZE =
     std.math.ceilPowerOfTwo(u32, @as(u32, @intCast(pkmn.gen1.LOGS_SIZE))) catch unreachable;
+
+export fn GEN1_update(
+    battle: *pkmn.gen1.Battle(pkmn.gen1.PRNG),
+    c1: pkmn.Choice,
+    c2: pkmn.Choice,
+    o: ?[*]u8,
+) pkmn.Result {
+    return (if (o) |opts| result: {
+        const buf = @as([*]u8, @ptrCast(opts))[0..pkmn.gen1.LOGS_SIZE];
+        var stream: pkmn.protocol.ByteStream = .{ .buffer = buf };
+        // TODO: extract out
+        var options = pkmn.battle.options(
+            pkmn.protocol.FixedLog{ .writer = stream.writer() },
+            pkmn.gen1.chance.NULL,
+            pkmn.gen1.calc.NULL,
+        );
+        break :result battle.update(c1, c2, &options);
+    } else battle.update(c1, c2, &pkmn.gen1.NULL)) catch unreachable;
+}
+
+export fn GEN1_choices(
+    battle: *pkmn.gen1.Battle(pkmn.gen1.PRNG),
+    player: u8,
+    request: u8,
+    out: [*]u8,
+) u8 {
+    assert(player <= @field(@typeInfo(pkmn.Player), @tagName(Enum)).fields.len);
+    assert(request <= @field(@typeInfo(pkmn.Choice.Type), @tagName(Enum)).fields.len);
+
+    const len = GEN1_CHOICES_SIZE;
+    return battle.choices(@enumFromInt(player), @enumFromInt(request), @ptrCast(out[0..len]));
+}
--- src/pkg/addon/wasm.ts
+++ src/pkg/addon/wasm.ts
@@ -1,4 +1,5 @@
 import type {Binding, Bindings} from '../addon';
+import {LAYOUT} from '../data';
 
 export function toBindings<T extends boolean>(w: WebAssembly.Exports): Bindings<T> {
   return {
@@ -8,13 +9,35 @@ export function toBindings<T extends boolean>(w: WebAssembly.Exports): Bindings<
       chance: w.CHANCE.valueOf(),
       calc: w.CALC.valueOf(),
     },
-    bindings: [toBinding('GEN1', w)],
+    bindings: [toBinding(1, w)],
   };
 }
 
-function toBinding(prefix: string, w: WebAssembly.Exports) {
+function toBinding(gen: number, w: WebAssembly.Exports): Binding {
+  const prefix = `GEN${gen}`;
+  const size = LAYOUT[gen - 1].sizes.Battle;
+  const update = w[`${prefix}_update`] as CallableFunction;
+  const choices = w[`${prefix}_choices`] as CallableFunction;
+  const memory = new Uint8Array((w.memory as WebAssembly.Memory).buffer);
+
   return {
     CHOICES_SIZE: w[`${prefix}_CHOICES_SIZE`].valueOf(),
     LOGS_SIZE: w[`${prefix}_LOGS_SIZE`].valueOf(),
-  } as unknown as Binding; // FIXME
+    update(battle: ArrayBuffer, c1: number, c2: number, log: ArrayBuffer | undefined): number {
+      memory.set(battle as any);
+      if (log) {
+        memory.set(log as any, size);
+        return update(0, c1, c2, size);
+      } else {
+        return update(0, c1, c2, 0);
+      }
+    },
+    choices(battle: ArrayBuffer, player: number, request: number, options: ArrayBuffer): number {
+      memory.set(battle as any);
+      const n = choices(0, player, request, size);
+      const data = new Uint8Array(options);
+      for (let i = 0; i < n; i++) data[i] = memory[size + i];
+      return n;
+    },
+  };
 }
