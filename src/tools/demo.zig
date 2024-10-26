48d071eaf05254232c79e7e163541123a18724b9
Kirk Scheibelhut <kjs@scheibo.com>
2024-10-25 23:00:31 -0700

    begin wiring up demo to be wasm-able

--- src/tools/demo.zig
+++ src/tools/demo.zig
@@ -6,8 +6,14 @@ const assert = std.debug.assert;
 const Choice = pkmn.Choice;
 const protocol = pkmn.protocol;
 const Rational = pkmn.Rational;
+const wasm = pkmn.bindings.wasm;
 
-pub const pkmn_options = pkmn.Options{ .advance = false, .ebc = false, .key = true };
+pub const pkmn_options = pkmn.Options{
+    .advance = false,
+    .ebc = false,
+    .key = true,
+    .internal = true,
+};
 
 const gen1 = struct {
     const Actions = pkmn.gen1.chance.Actions;
@@ -154,3 +160,18 @@ const gen1 = struct {
         p.reduce();
     }
 };
+
+export const SHOWDOWN = wasm.options.showdown;
+export const LOG = wasm.options.log;
+export const CHANCE = wasm.options.chance;
+export const CALC = wasm.options.calc;
+
+export const GEN1_CHOICES_SIZE = wasm.gen(1).CHOICES_SIZE;
+export const GEN1_LOGS_SIZE = wasm.gen(1).LOGS_SIZE;
+
+const exportable = @hasDecl(std.zig, "Zir") and !@hasDecl(std.zig.Zir.Inst, "export_value");
+
+usingnamespace if (exportable) struct {
+    export const GEN1_update = wasm.gen(1).update;
+    export const GEN1_choices = wasm.gen(1).choices;
+} else wasm.exports();
