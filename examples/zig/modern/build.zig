f9bef37c8392b4ac396da067e70275c268e88f67
fedejinich <federico@rootstocklabs.com>
2024-11-20 12:03:34 -0300

    zig.build cleanup

--- examples/zig/modern/build.zig
+++ examples/zig/modern/build.zig
@@ -1,25 +1,21 @@
 const std = @import("std");
-const CrossTarget = std.zig.CrossTarget;
+const CrossTarget = @import("std").zig.CrossTarget;
+const Target = @import("std").Target;
+const Feature = Target.Cpu.Feature;
+// pub const featureSet = Target.Cpu.Feature.feature_set_fns(Feature).featureSet;
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const riscv_target = CrossTarget{
-        // the cpu architecture I intend to produce binaries for
         .cpu_arch = .riscv32,
-
-        // the operating system to compile the code for, we specify
+        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
+        .abi = .gnu,
+        // operating system to compile for, we specify
         // `freestanding` over here because this doesn't really run
         // on any operating system zig is aware of.
+        // todo(fede) do we need freestanding?
         .os_tag = .freestanding,
-
-        // I still don't really understand this.
-        // .abi = .gnueabihf,
-        .abi = .gnu,
-
-        // the cpu model includes the features of the cpu architecture
-        // the hardware manufacturers added to their product.
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
     };
 
     const resolved_target = b.resolveTargetQuery(riscv_target);
@@ -45,24 +41,3 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("pkmn", pkmn.module("pkmn"));
     b.installArtifact(exe);
 }
-
-// pub fn build(b: *std.Build) void {
-//     const target = std.zig.CrossTarget{
-//         .cpu_arch = std.Target.Cpu.Arch.riscv32,
-//         .os_tag = std.Target.Os.Tag.freestanding,
-//     };
-//
-//     // Standard release options allow the person running `zig build` to select
-//     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-//     const mode = b.standardOptimizeOption(.{});
-//
-//     const exe = b.addExecutable(.{
-//         .
-//     }"nosering", "dummy_battle.zig");
-//     exe.code_model = .medium;
-//     // exe.addAssemblyFile("src/entry.S");
-//     exe.setLinkerScriptPath(std.build.FileSource{ .path = "link.ld" });
-//     exe.setTarget(target);
-//     exe.setBuildMode(mode);
-//     exe.install();
-// }
