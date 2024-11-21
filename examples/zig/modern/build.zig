4a2fae8f8d48784d64e21f6ab01d4e061fb7d64a
fedejinich <federico@rootstocklabs.com>
2024-11-21 19:17:14 -0300

    code cleanup

--- examples/zig/modern/build.zig
+++ examples/zig/modern/build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const CrossTarget = @import("std").zig.CrossTarget;
 const Target = @import("std").Target;
 const Feature = Target.Cpu.Feature;
-// pub const featureSet = Target.Cpu.Feature.feature_set_fns(Feature).featureSet;
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
@@ -14,25 +13,13 @@ pub fn build(b: *std.Build) void {
     enabled_features.addFeature(@intFromEnum(features.i));
     enabled_features.addFeature(@intFromEnum(features.m));
 
-    // var disabled_features = Feature.Set.empty;
-    // disabled_features.addFeature(@intFromEnum(features.a));
-    // disabled_features.addFeature(@intFromEnum(features.c));
-    // disabled_features.addFeature(@intFromEnum(features.d));
-    // disabled_features.addFeature(@intFromEnum(features.e));
-    // disabled_features.addFeature(@intFromEnum(features.f));
-
     // setup riscv32im target
     const riscv_target = CrossTarget{
         .cpu_arch = .riscv32,
         .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
         .abi = .gnu,
-        // operating system to compile for, we specify
-        // `freestanding` over here because this doesn't really run
-        // on any operating system zig is aware of.
-        // todo(fede) do we need freestanding?
-        .os_tag = .freestanding,
+        .os_tag = .freestanding, // this doesn't really run on any operating system zig is aware of
         .cpu_features_add = enabled_features,
-        // .cpu_features_sub = disabled_features,
     };
     const resolved_target = b.resolveTargetQuery(riscv_target);
 
@@ -41,6 +28,7 @@ pub fn build(b: *std.Build) void {
         b.option(bool, "showdown", "Enable Pokémon Showdown compatibility mode") orelse false;
     const log = b.option(bool, "log", "Enable protocol message logging") orelse false;
 
+    // build elf
     const exe = b.addExecutable(.{
         .name = "pkmn_battle",
         .root_source_file = if (@hasField(std.Build.LazyPath, "path"))
